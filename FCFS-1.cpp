38df284a2bf5c1fb4407ce5159e2bf2be66a63d8
Asik8 <motasembillah84@gmail.com>
2024-10-25 22:05:38 +0600

    First Code First Serve Implementation

--- FCFS-1.cpp
+++ FCFS-1.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 #define ll long long
 #define pb push_back
-#define py cout<<"YES\n";
-#define pn cout<<"NO\n";
-#define forni for (int i = 0; i < n; i++)
-#define fornj for (int j = 0; j < n; j++)
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--) {
-        ll n;
-        cin >> n;
-        vector <ll> v(n);
-        for (auto& x:v) cin >>x;       
-    }
+    vector<ll> process={1,2,3};
+    vector<ll> bt={24,3,3};
+    vector<ll> wt(3),tat(3);
+    wt[0] = 0;
+    for(int i=1;i<3;i++) wt[i] = bt[i-1]+wt[i-1];
+    for(int i=0;i<3;i++) tat[i] = bt[i]+wt[i];
+    cout<<"Waiting Time:\n";
+    for(ll c:wt) cout<<c<<" ";
+    cout<<endl;
+    cout<<"Turn Around Time:\n";
+    for(ll c:tat) cout<<c<<" ";
+    cout<<endl;
     return 0;
 }
